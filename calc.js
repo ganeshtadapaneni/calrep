d4e56739e7555b12f0c80b42f70fdfac9144350d
ganeshtadapaneni <128682357+ganeshtadapaneni@users.noreply.github.com>
2023-03-23 13:45:27 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,12 @@
+function add(x,y){
+    return x+y;
+};
+function sub(x,y){
+     return x-y;
+};
+function mult(x,y){
+    return x*y;
+};
+exports.addition=add;
+exports.subtract=sub;
+exports.multiply=mult;
